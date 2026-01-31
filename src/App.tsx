caee7c53df17737c64a3b35eea74aa2df148808a
evenordinstorm <evenordinstorm@gmail.com>
2026-01-31 22:19:52 +0000

    feat: footer component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Home/HomePage";
 import ResourcePage from "./pages/Resource/ResourcePage";
+import Footer from "./components/footer";
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/resource/:id" element={<ResourcePage />} />
       </Routes>
+
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,23 @@
+export default function Footer() {
+  return (
+    <div className="text-center pt-10">
+      <a
+        href="https://www.bdadyslexia.org.uk/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="font-bold text-active-blue underline text-lg mb-1 inline-block hover:text-core-blue"
+      >
+        bdadyslexia.org.uk
+      </a>
+      <div className="text-xs text-core-blue">
+        © British Dyslexia Association
+      </div>
+      <div className="text-xs text-core-blue">
+        British Dyslexia Association is a registered charity in England and
+        Wales (289243). Registered in England and Wales as a company limited by
+        guarantee (1830587). Registered office: 1, Friar Street, Reading,
+        England, RG1 1DA
+      </div>
+    </div>
+  );
+}
